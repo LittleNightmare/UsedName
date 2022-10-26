38bdc14fcd55bf281fd93d23ca499e61925b95a2
LittleNightmare <littlenightmare2000@gmail.com>
2022-10-26 13:46:20 -0400

    fix potential transfer data issues

--- UsedName/Configuration.cs
+++ UsedName/Configuration.cs
@@ -5,14 +5,17 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.Encodings.Web;
+using System.Text;
 using System.Text.Json;
+using System.Linq;
 
 namespace UsedName
 {
     [Serializable]
     public class Configuration : IPluginConfiguration
     {
-        public int Version { get; set; } = 1;
+        public int Version { get; set; } = 2;
 
         public string? Language = null;
 
@@ -80,11 +83,18 @@ namespace UsedName
                     if (playersNameList.Equals(new Dictionary<ulong, PlayersNames>()))
                     {
                         playersNameList = System.Text.Json.JsonSerializer.Deserialize<Dictionary<ulong, PlayersNames>>(json);
-
+                    }
+                    else
+                    {
+                        var temp = System.Text.Json.JsonSerializer.Deserialize<Dictionary<ulong, PlayersNames>>(json);
+                        if(temp != null&& !temp.Equals((new Dictionary<ulong, PlayersNames>())))
+                        {
+                            playersNameList = mergePlayerList(playersNameList, temp);
+                        }
                     }
                 }
             }
-
+            Save(storeName: true);
         }
 
         public void Save(bool storeName = false)
@@ -98,11 +108,50 @@ namespace UsedName
 
         public void storeNames()
         {
-            string jsonString = System.Text.Json.JsonSerializer.Serialize(playersNameList, new JsonSerializerOptions() { WriteIndented = true });
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(playersNameList, new JsonSerializerOptions() { WriteIndented = true, Encoder= JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
             using (StreamWriter outputFile = new StreamWriter(storeNamesPath, false, System.Text.Encoding.UTF8))
             {
                 outputFile.WriteLine(jsonString);
             }
+        }
+
+        internal IDictionary<ulong, PlayersNames> mergePlayerList(IDictionary<ulong, PlayersNames> main, IDictionary<ulong, PlayersNames> sub)
+        {
+            var result = main;
+            foreach(var item in sub)
+            {
+                if (!result.ContainsKey(item.Key))
+                {
+                    result.Add(item.Key, item.Value);
+                }else if(result[item.Key] == item.Value)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (result[item.Key].currentName != item.Value.currentName)
+                    {
+                        if(item.Value.usedNames.Contains(result[item.Key].currentName))
+                        {
+                            result[item.Key] = item.Value;
+                        }
+
+                    }
+                    var tempUsedName = result[item.Key].usedNames;
+                    tempUsedName.AddRange(item.Value.usedNames);
+                    result[item.Key].usedNames = tempUsedName.Distinct().ToList();
+                    if (result[item.Key].nickName != item.Value.nickName)
+                    {
+                        if (String.IsNullOrEmpty(result[item.Key].nickName))
+                        {
+                            result[item.Key].nickName = item.Value.nickName;
+                        }
+
+                        // use result[item.Key].nickName in other situations
+                    }
+                }
+            }
+            return result;
         }
     }
 }
